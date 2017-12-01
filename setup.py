083aedcc7edd096b48b33b0955a62d901898a960
macoskey <macoskey@umich.edu>
2017-12-01 12:27:25 -0500

    update

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+# Useful functions for common image/data processing methods performed 
+# in the Image-Guided Ultrasound Therapy (aka Histotripsy) lab at the
+# University of Michigan Department of Biomedical Engineering
+#
+# setup.py file
+#
+#
+# Curated by Jonathan J. Macoskey
+# https://github.com/macoskey
+#
+# First created: 12.1.17
 
+from setuptools import setup
+
+setup(name='histotripsy',
+      version='0.1',
+      description='Useful Functions for the Image-Guided Ultrasound Therapy Lab',
+      url='https://github.com/macoskey/histotripsy',
+      author='Jonathan Macoskey',
+      author_email='macoskey@umich.edu',
+      license='Michigan',
+      packages=['histotripsy'],
+      zip_safe=False)
