cda570a0eb4c8fda1da0704664b3d3d71aef707f
macoskey <macoskey@umich.edu>
2017-12-01 11:53:45 -0500

    first commit

---
+++ histotripsy/histotripsy/__init__.py
@@ -0,0 +1,14 @@
+# Useful functions for common image/data processing methods performed 
+# in the Image-Guided Ultrasound Therapy (aka Histotripsy) lab at the
+# University of Michigan Department of Biomedical Engineering
+#
+# __init__.py file
+#
+#
+# Curated by Jonathan J. Macoskey
+# https://github.com/macoskey
+#
+# First created: 12.1.17
+
+def firstFunction():
+	print "Test"
